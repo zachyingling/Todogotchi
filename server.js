6c26b735e6feb9617606aa6e6a3fbb0e8ce3a295
zachary <zpyingling9559@gmail.com>
2020-05-12 21:33:48 -0400

    added 4 routes (1 get route for the home route /, and 3 post routes, 1 for login, 1 for logout, and 1 for createAccout)

--- server.js
+++ server.js
@@ -27,9 +27,33 @@ app.use(session({
   saveUninitialized: true,
   store: sessionStore,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (tbh idk the math)
+    maxAge: 1000 * 60 * 60 * 24 // Equals 1 day in milliseconds
   }
 }));
+
+app.get("/", (req, res) => {
+  // Start session here
+  res.end();
+});
+
+app.post("/create", (req, res) => {
+  // We can setup this route to make a new account; tests to see if the username/email exists in the database; if the username/email doesn't exist create account else tell user that you can't use username/email
+});
+
+app.post("/login", (req, res) => {
+  // We can setup this route to make a call to the database to see if the username and password exists, then if it exists login and start the session;
+});
+
+app.post("/logout", (req, res) => {
+  // Post route destroys the session connection
+  req.session.destroy((err) => {
+    if (err) {
+      return res.send(false);
+    }
+    res.clearCookie("project2");
+    res.send(true);
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Listening at:  localhost:" + PORT);
