eac9590a6dc3337497b8fa0952ee739161a1e034
beardykraken <vcasmirri@gmail.com>
2020-05-15 19:10:31 -0400

    Began configuring Pet.js state

--- client/src/components/Pet.js
+++ client/src/components/Pet.js
@@ -1,2 +1,9 @@
 import React from "react";
 import "./Pet.css";
+
+this.state = {
+    animationState: spriteAnimations.IDLE,
+
+    wellbeingStats: {happiness: 100, energy: 0, mood: 100, pawPoints: 100}
+    
+  };
