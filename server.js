49b2cdee2c28df04e5d8e43b1b73c7bb4ed670e4
zachary <zpyingling9559@gmail.com>
2020-05-15 19:42:26 -0400

    adding the necessary checks in the routes to login/logout/create accounts in session

--- server.js
+++ server.js
@@ -34,17 +34,33 @@ app.use(session({
 app.get("/", (req, res) => {
   // Start session here
   sess = req.session;
-  res.end();
+
+  if(sess.email){
+    res.send(true); // tells the react router to route to /home route
+  } else {
+    res.send(false); // tells the react router to route to / route with err message
+  }
 });
 
 app.post("/create", (req, res) => {
-  // We can setup this route to make a new account; tests to see if the username/email exists in the database; if the username/email doesn't exist create account else tell user that you can't use username/email
   sess = req.session;
+
+  // \/  Do a db search for req.body.email and req.body.password if its created yet. then create
+  sess.email = req.body.email;
+  sess.password = req.body.password;
+
+  res.end();
 });
 
 app.post("/login", (req, res) => {
   // We can setup this route to make a call to the database to see if the username and password exists, then if it exists login and start the session;
   sess = req.session;
+
+  if(sess.email && sess.password) {
+    res.send(true);
+  } else {
+    res.send(false);
+  }
 });
 
 app.post("/logout", (req, res) => {
@@ -54,8 +70,8 @@ app.post("/logout", (req, res) => {
     if (err) {
       return res.send(false);
     }
-    res.clearCookie("project2");
-    res.send(true);
+    res.clearCookie();
+    return res.send(true);
   });
 });
 
