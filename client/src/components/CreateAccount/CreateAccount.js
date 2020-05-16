58e4b73eeac0775b9fc37c000144312d1cfbb05e
zachary <zpyingling9559@gmail.com>
2020-05-16 13:22:07 -0400

    getting the modal to show on button click

--- client/src/components/CreateAccount/CreateAccount.js
+++ client/src/components/CreateAccount/CreateAccount.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Modal } from "react-bootstrap";
 
-function CreateAccountButton () {
+function CreateAccount () {
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <button className="createAccount">Create Account</button>
+    <>
+      <button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={handleClose}>
+            Close
+          </button>
+          <button variant="primary" onClick={handleClose}>
+            Save Changes
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
-export default CreateAccountButton;
+export default CreateAccount;
