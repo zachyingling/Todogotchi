5294b4407dda36dd84274aaf4ef4d8272b710fea
beardykraken <vcasmirri@gmail.com>
2020-05-16 17:36:23 -0400

    configured happiness and energy state within PetWindow component, added Bootstrap component shortcuts

--- client/src/components/PetWindow/PetWindow.js
+++ client/src/components/PetWindow/PetWindow.js
@@ -1,3 +1,61 @@
-import React from "react";
-import "./Pet.css";
+import React, { Component } from "react";
+// import "../PetWindow/Petwindow.css";
+import "../Sprite/Sprite";
+import { Col, Row, Container } from "../Grid";
+import Jumbotron from "../Jumbotron";
+import happy from "../../images/sprites/phoebe-affection-happy.gif";
 
+
+class PetWindow extends Component {
+    state = {
+        animationState: 0,
+        happiness: 0,
+        energy: 0
+    };
+
+    componentDidMount() {
+        this.setState({ happiness: 8, energy: 8});
+
+        // this will likely need to have some kind of .then function - get current state from database, .then load database stat values accordingly. otherwise it will reflect default state set above and then switch.
+        console.log("Happiness is currently: " + this.state.happiness);
+        console.log("Energy is currently: " + this.state.energy);
+    };
+    
+    
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1>Happiness: {this.state.happiness}</h1>
+                            <h1>Energy: {this.state.energy}</h1>
+                        </Jumbotron>
+                    </Col>
+                    <Col size="md-12">
+                        <img src={happy} alt="Happy Pet" />
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+export default PetWindow;
