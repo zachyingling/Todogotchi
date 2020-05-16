9a03fe968dabd1642858a7b7564411cc0d109451
zachary <zpyingling9559@gmail.com>
2020-05-15 20:20:00 -0400

    basic file structure for create account components

---
+++ client/src/components/CreateAccountModel/CreateAccountModel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CreateAccountModel extends React.Component {
+  render () {
+    return(
+      <form>
+        
+      </form>
+    );
+  }
+}
+
+export default CreateAccountModel;
