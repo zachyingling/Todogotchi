d164a592acbca2d286bcff1e578dd76267404ea4
Ben Mulholland <ben.mulholland@gmail.com>
2020-05-28 18:45:39 -0400

    css to logo mostly and some in app.css

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import HowToPlay from "../HowToPlay/HowToPlay.js"
+import "./Logo.css";
 
 
 const Logo = props => {
     return(
         <div>
             <h1>
-                TODOGotchi
+                TODO-Gotchi
             </h1>
             <HowToPlay/>
         </div>
